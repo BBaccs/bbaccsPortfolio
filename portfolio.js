da012783e69f8f5f70b328c99e5822d7020222f4
Brian <baccarellab@gmail.com>
2019-05-20 11:37:03 -0400

    style portfolio images create js alert
    created an. alert for when you click on the react logo
    
    styled portfolio images so they don't blow up too big on 30+ inch screens

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,4 @@
-//set vars
+//Set Variables
 var btnJavaApp = document.querySelector('#btnJavaApp');
 var javascriptApp = document.querySelectorAll('.portfolioItem:not(.javascript');
 var btnTemplate = document.querySelector('#btnTemplate');
@@ -7,18 +7,22 @@ var btnShowAll = document.querySelector('#btnAll');
 var btnRaw = document.querySelector("#btnRaw");
 var rawCssSite = document.querySelectorAll('.portfolioItem:not(.rawCSS)');
 var portfolioItems = document.querySelectorAll('.portfolioItem');
+var btnReact = document.querySelector("#btnReactApp");
 
-//set functionS
 
+//Set functions
 function showAllPortfolioItems(){
     for(i = 0; i < portfolioItems.length; i++){
         portfolioItems[i].classList.remove('hide');
     }
 }
 
+function showAlert(){
 
-//events
+}
 
+
+//Set Event Listeners
 btnShowAll.addEventListener('click', function () {
     showAllPortfolioItems();
 })
@@ -45,10 +49,29 @@ btnRaw.addEventListener('click', function () {
     }
 })
 
+btnReact.addEventListener('click', function (){
+    //create div
+    const div = document.createElement('div');
+    const section = document.querySelector('#portfolio');
+    const portfolioImages = document.getElementById('portfolio-images');
+    //insert html
+    div.className = 'reactAlert';
+
+    //add text
+    div.appendChild(document.createTextNode('React work coming soon, for now check out my Vanilla JS Work'));
+    
+    //insert before ul
+    section.insertBefore(div, portfolioImages);
+
+    //only display alert for 3 seconds
+    setTimeout(function(){
+        document.querySelector('.reactAlert').remove();
+    }, 6000);
+});
 
 
 
-// working jQuery code
+// Keep old jQuery code here
 
 // $('#btnJavaApp').on('click', function(){
 // 	$('ul#portfolio-images li').removeClass('hide');
