7db7ad56d46381dd99ecc0938bdef41b680746a6
Brian <baccarellab@gmail.com>
2019-06-03 13:47:20 -0400

    remove unsused func and comments add new img

--- portfolio.js
+++ portfolio.js
@@ -17,10 +17,6 @@ function showAllPortfolioItems(){
     }
 }
 
-function showAlert(){
-
-}
-
 
 //Set Event Listeners
 btnShowAll.addEventListener('click', function () {
@@ -68,92 +64,3 @@ btnReact.addEventListener('click', function (){
         document.querySelector('.reactAlert').remove();
     }, 6000);
 });
-
-
-
-// Keep old jQuery code here
-
-// $('#btnJavaApp').on('click', function(){
-// 	$('ul#portfolio-images li').removeClass('hide');
-// 	$('ul#portfolio-images li.usedTemplate').addClass('hide');
-// 	$('ul#portfolio-images li.rawCss').addClass('hide');
-// })
-
-// $('#btnTemplate').on('click', function() {
-// 	$('ul#portfolio-images li').removeClass('hide');
-// 	$('ul#portfolio-images li.rawCss').addClass('hide');
-// 	$('ul#portfolio-images li.jsApp').addClass('hide');
-// })
-
-// $('#btnAll').on('click', function(){
-// 	$('ul#portfolio-images li').removeClass('hide');
-// })
-
-// $('#rawCss').on('click', function(){
-// 	$('ul#portfolio-images li').removeClass('hide');
-// 	$('ul#portfolio-images li.jsApp').addClass('hide');
-// 	$('ul#portfolio-images li.usedTemplate').addClass('hide');
-// })
-
-
-
-// ORIGINAL JAVASCRIPT 
-//set vars
-// var btnJavaApp = document.querySelector('#btnJavaApp');
-// var noJava = document.getElementsByClassName('noJS');
-// var btnTemplate = document.querySelector('#btnTemplate');
-// var template = document.getElementsByClassName('noTemplate');
-// var btnShowAll = document.querySelector('#btnAll');
-// var btnRaw = document.querySelector("#btnRaw");
-// var rawCSS = document.querySelectorAll('.portfolioItem:not(.rawCSS)');
-
-
-
-// btnShowAll.addEventListener('click', function () {
-// 	showPortfolioItems();
-// })
-
-// btnRaw.addEventListener('click', function(){
-// 	showPortfolioItems();
-// 	for (var i = 0; i < rawCSS.length; i++) {
-// 		rawCSS[i].classList.add('hide');
-// 	}
-// })
-
-// btnJavaApp.addEventListener('click', function(){
-// 	showPortfolioItems();
-// 	for (var i = 0; i < noJava.length; i++) {
-// 		noJava[i].classList.add('hide');
-// 	}
-// })
-
-// btnTemplate.addEventListener('click', function(){
-// 	showPortfolioItems();
-// 	for (var i = 0; i < template.length; i++) {
-// 		template[i].classList.add('hide');
-// 	}
-// })
-
-
-//declare functions
-// function showPortfolioItems() {
-// 	var portfolioItem = document.getElementsByClassName('portfolioItem');
-// 	for (var i = 0; i < portfolioItem.length; i++) {
-// 		portfolioItem[i].classList.remove('hide');
-// 	}
-// }
-
-
-
-
-
-		// Testing Mobile Navbar
-
-// var button = document.querySelector("button");
-// var paragraph = document.querySelector("p");
-
-//setup
-// button.addEventListener("click", function() {
-// 	paragraph.classList.toggle("hide");
-// })
-
